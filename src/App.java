cf06d3aef803c62f4b231e89d66556475dc9bd22
Jazmin <programajazminc20@gmail.com>
2025-10-01 14:22:51 +0200

    feat: upload address data

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nombre = "Jazmin Gutierrez Castro";
+        String direccion = "Calle 5 de Febrero #123, Col. Centro, CDMX";
+        String telefono = "5551234567";
+
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
     }
 }
